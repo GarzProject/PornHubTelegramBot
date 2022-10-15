3079f67467a699d2838a10c3ce92719dc51c73e5
Tegar Prayuda <115115864+GarzProject@users.noreply.github.com>
2022-10-15 20:38:29 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# variable
+APP_ID = "" # app id my.telegram.org
+APP_HASH = "" # app hash my.telegram.org
+BOT_TOKEN = "" # token bot dari @botfather
+SUDO = "" # id admin dari bot
+MUST_JOIN = "GarzProject" # username channel publik untuk fsub
